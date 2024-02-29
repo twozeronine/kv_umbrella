f2220ea9100a7834d970e9d2607241526ad0476b
twozeronine <amdusias209@gmail.com>
2024-02-29 14:00:05 +0900

    Implement using supervisor for multiply connections

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -3,7 +3,12 @@ defmodule KVServer.Application do
 
   @impl true
   def start(_type, _args) do
-    children = []
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
+    children = [
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
+    ]
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
